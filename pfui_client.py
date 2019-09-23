da9bb4aa8fd37fa9c30c65eebe749d110efee94b
Andy <andrewl@4d-dc.com>
2019-09-23 16:52:24 +0100

    pf_client.py clean up

--- pfui_client.py
+++ pfui_client.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 """
+Written using example python module scripts in Unbound source.
+
 :param qinfo: query_info struct;
 :param qstate: module qstate. None;
 :param rep: reply_info struct;
@@ -20,7 +22,9 @@ from json import dumps
 
 
 def dataHex(data, prefix=""):
-    """ Converts binary string data to display form """
+    """ Converts binary string data to display form.
+        Function taken from Unbound examples. """
+
     res = ""
     for i in range(0, (len(data)+15)/16):
         res += "%s0x%02X | " % (prefix, i*16)
@@ -41,6 +45,7 @@ def dataHex(data, prefix=""):
 
 def logger(qstate):
     """ Logs Response """
+
     r = qstate.return_msg.rep
     q = qstate.return_msg.qinfo
     print("-"*100)
@@ -49,7 +54,7 @@ def logger(qstate):
             str(qstate.qinfo.qclass_str), str(qstate.qinfo.qclass)))
     print("-"*100)
     print("Return    reply :: flags: {}, QDcount: {}, Security:{}, TTL={}".format(str(r.flags), str(r.qdcount),
-                                                                                     str(r.security), str(r.ttl)))
+                                                                                  str(r.security), str(r.ttl)))
     print("          qinfo :: qname: {} {}, qtype: {}, qclass: {}".format(str(q.qname_list), str(q.qname_str),
                                                                           str(q.qtype_str), str(q.qclass_str)))
     if r:
@@ -77,7 +82,7 @@ def logger(qstate):
 
 
 def read_rr(qstate=None, rep=None):
-    """ Inspects the RR Data, extracts IPs and TTLs, and generates structure for transmit()
+    """ Inspects the RR Data, extracts IPs and TTLs, and populates data structure.
         Data Structure: {'AF4': [{"ip": ipv4_addr, "ttl": ip_ttl }], 'AF6': [{"ip": ipv6_addr, "ttl": ip_ttl }]} """
 
     ipv4, ipv6 = [], []
@@ -88,7 +93,8 @@ def read_rr(qstate=None, rep=None):
     if r:
         for i in range(0, r.rrset_count):
             rr = r.rrsets[i]
-            if cfg['DEBUG']:  log_info("DEBUG: r.rrsets[{}]: rr.rk.type_str {}".format(str(i), str(rr.rk.type_str)))
+            if cfg['DEBUG']:
+                log_info("DEBUG: r.rrsets[{}]: rr.rk.type_str {}".format(str(i), str(rr.rk.type_str)))
             if rr.rk.type_str == 'A':
                 d = rr.entry.data
                 for j in range(0, d.count+d.rrsig_count):
@@ -96,9 +102,10 @@ def read_rr(qstate=None, rep=None):
                     try:
                         ip = socket.inet_ntop(socket.AF_INET, rr_ip)  # IP bytes to display format
                         ipv4.append({"ip": ip, "ttl": int(d.rr_ttl[j])})
-                        if cfg['DEBUG']:  log_info("DEBUG: Found IPv4 address %s" % (ipv4[-1]))
+                        if cfg['DEBUG']:
+                            log_info("DEBUG: Found IPv4 address {}".format(str(ipv4[-1])))
                     except:
-                        log_info("ERROR: Invalid IPv4 address %s" % ip)
+                        log_info("ERROR: Invalid IPv4 address {}".format(str(ip)))
             elif rr.rk.type_str == 'AAAA':
                 d = rr.entry.data
                 for j in range(0, d.count+d.rrsig_count):
@@ -106,9 +113,10 @@ def read_rr(qstate=None, rep=None):
                     try:
                         ip = socket.inet_ntop(socket.AF_INET6, rr_ip)
                         ipv6.append({"ip": ip, "ttl": int(d.rr_ttl[j])})
-                        if cfg['DEBUG']:  log_info("DEBUG: Found IPv6 address %s" % (ipv6[-1]))
+                        if cfg['DEBUG']:
+                            log_info("DEBUG: Found IPv6 address {}".format(str(ipv6[-1])))
                     except:
-                        log_info("ERROR: Invalid IPv6 address %s" % ip)
+                        log_info("ERROR: Invalid IPv6 address {}".format(str(ip)))
 
         if ipv4 or ipv6:
             return {'AF4': ipv4, 'AF6': ipv6}
@@ -117,15 +125,17 @@ def read_rr(qstate=None, rep=None):
 
 
 def transmit(ip_dict):
-    """ Transmits IPs and TTLs to PF Firewall running pfui_server listener. """
+    """ Transmits IP and TTL data structure to PF Firewalls running pfui_server. """
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, cfg['SOCKET_TIMEOUT'])
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
     for fw in cfg['FIREWALLS']:
         if fw['HOST']:
-            if not fw['PORT']: fw['PORT'] = cfg['DEFAULT_PORT']
+            if not fw['PORT']:
+                fw['PORT'] = cfg['DEFAULT_PORT']
             try:
-                if cfg['DEBUG']:  log_info("DEBUG: SENDING DATA: {} {}".format(str(type(ip_dict)), str(ip_dict)))
+                if cfg['DEBUG']:
+                    log_info("DEBUG: SENDING DATA: {} {}".format(str(type(ip_dict)), str(ip_dict)))
                 s.connect((fw['HOST'], fw['PORT']))
                 s.send(dumps(ip_dict))
             except Exception as e:
@@ -136,7 +146,8 @@ def transmit(ip_dict):
 def inplace_cache_callback(qinfo, qstate, rep, rcode, edns, opt_list_out, region, **kwargs):
     """ Inplace callback function for cache responses. """
 
-    if cfg['DEBUG']:  log_info("DEBUG: pythonmod: cache_callback called while answering from cache.")
+    if cfg['DEBUG']:
+        log_info("DEBUG: pythonmod: cache_callback called while answering from cache.")
     if rep:
         struct = read_rr(rep=rep)
     if struct:
@@ -145,8 +156,10 @@ def inplace_cache_callback(qinfo, qstate, rep, rcode, edns, opt_list_out, region
 
 
 def init(id, cfg):
-    if cfg['DEBUG']:  log_info("DEBUG: pythonmod: init called, module id is %d port: %d script: %s"
-                               % (id, cfg.port, cfg.python_script))
+    if cfg['DEBUG']:
+        log_info("DEBUG: pythonmod: init called, module id {} port: {} script: {}".format(str(id),
+                                                                                          str(cfg.port),
+                                                                                          str(cfg.python_script)))
     return True
 
 
@@ -154,31 +167,36 @@ def init_standard(id, env):
     """ Register inplace_cache_callback() as the callback function for inspecting cache responses.
         (Iterator is not called for cache responses). """
 
-    if cfg['DEBUG']:  log_info("DEBUG: pythonmod: init_standard called, module id is %d port: %d script: %s"
-                               % (id, env.cfg.port, env.cfg.python_script))
+    if cfg['DEBUG']:
+        log_info("DEBUG: pythonmod: init_standard called, module id {} port: {} script: {}".format(str(id),
+                                                                                          str(cfg.port),
+                                                                                          str(cfg.python_script)))
     if not register_inplace_cb_reply_cache(inplace_cache_callback, env, id):
         return False
     return True
 
 
 def deinit(id):
-    if cfg['DEBUG']:  log_info("DEBUG: pythonmod: deinit called, module id is %d" % id)
+    if cfg['DEBUG']:
+        log_info("DEBUG: pythonmod: deinit called, module id {}".format(str(id)))
     return True
 
 
 def inform_super(id, qstate, superqstate, qdata):
-    if cfg['DEBUG']:  log_info("DEBUG: pythonmod: inform_super called, module is is %d. qstate is %s"
-                               % (id, str(qstate)))
+    if cfg['DEBUG']:
+        log_info("DEBUG: pythonmod: inform_super called, module is is {}. qstate is {}".format(str(id), str(qstate)))
     return True
 
 
 def operate(id, event, qstate, qdata):
     """ When event == 'MODULE_EVENT_MODDONE' (Iterator finished) inspect RR response. """
 
-    if cfg['DEBUG']:  log_info("DEBUG: pythonmod: operate called, id: %d, event:%s" % (id, strmodulevent(event)))
+    if cfg['DEBUG']:
+        log_info("DEBUG: pythonmod: operate called, id: {}, event:{}".format(str(id), str(strmodulevent(event))))
 
     if event == MODULE_EVENT_NEW:
-        if cfg['DEBUG']:  log_info("DEBUG: pythonmod: MODULE_EVENT_NEW")
+        if cfg['DEBUG']:
+            log_info("DEBUG: pythonmod: MODULE_EVENT_NEW")
         qstate.ext_state[id] = MODULE_WAIT_MODULE
         return True
 
@@ -192,15 +210,18 @@ def operate(id, event, qstate, qdata):
             transmit(struct)
 
         qstate.ext_state[id] = MODULE_FINISHED
-        if cfg['DEBUG']:  log_info("DEBUG: pythonmod: MODULE_FINISHED")
+        if cfg['DEBUG']:
+            log_info("DEBUG: pythonmod: MODULE_FINISHED")
         return True
 
     if event == MODULE_EVENT_PASS:
-        if cfg['DEBUG']:  log_info("DEBUG: pythonmod: MODULE_EVENT_PASS")
+        if cfg['DEBUG']:
+            log_info("DEBUG: pythonmod: MODULE_EVENT_PASS")
         qstate.ext_state[id] = MODULE_WAIT_MODULE
         return True
 
-    if cfg['DEBUG']:  log_err("DEBUG: pythonmod: MODULE_ERROR")
+    if cfg['DEBUG']:
+        log_err("DEBUG: pythonmod: MODULE_ERROR")
     qstate.ext_state[id] = MODULE_ERROR
     return True
 
